eebb07e2847e4479f06f2f15cc0d33087148227b
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-09-26 15:26:01 +0200

    Trying to create scrolling

--- Assets/Prototyping/Pavel 1/Script/scrip.cs
+++ Assets/Prototyping/Pavel 1/Script/scrip.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class scrip : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private void Start()
     {
-        
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement;
+        lastCameraPosition = cameraTransform.position;
     }
 }
+
